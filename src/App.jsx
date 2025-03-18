4313e6186075c85b1c65565c187ca697da7488c8
climbingirl <isvetlanaivanova@yandex.ru>
2025-03-19 00:21:10 +0500

    feat: add value input and input error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [list, setList] = useState([]);
+  const [error, setError] = useState('');
+  const isValueVaild = value.length >= 3;
+
+  const onInputButtonClick = () => {
+    const promptValue = prompt('Введите значение', '').trim();
+    if (promptValue.length >= 3) {
+      setValue(promptValue);
+      setError('');
+    } else {
+      setError('Введенное значение должно содержать минимум 3 символа');
+    }
+  };
+
   return (
     <div className={styles.app}>
       <h1 className={styles['page-heading']}>Ввод значения</h1>
       <p className={styles['no-margin-text']}>
         Текущее значение <code>value</code>: "
-        <output className={styles['current-value']}></output>"
+        <output className={styles['current-value']}>{value}</output>"
       </p>
-      <div className={styles.error}>
-        Введенное значение должно содержать минимум 3 символа
-      </div>
+      {error !== '' ? <div className={styles.error}>{error}</div> : null}
       <div className={styles['buttons-container']}>
-        <button className={styles.button}>Ввести новое</button>
-        <button className={styles.button} disabled>
+        <button className={styles.button} onClick={onInputButtonClick}>
+          Ввести новое
+        </button>
+        <button className={styles.button} disabled={!isValueVaild}>
           Добавить в список
         </button>
       </div>
